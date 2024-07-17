f3e93a7e46316b143e0645fd67ae8c363ce5185b
Zoon <zoon_p@thinkingmachin.es>
2024-07-17 15:29:05 +0700

    Update __init__.py

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,5 +1,5 @@
 from .process import tokenize_text
-from .english import EnglishTextNormalizer 
-from .basic import BasicTextNormalizer 
+from .english import EnglishTextNormalizer as EnglishTextNormalizer
+from .basic import BasicTextNormalizer as BasicTextNormalizer
 from .postfix import remove_longest_repeating_words
 
